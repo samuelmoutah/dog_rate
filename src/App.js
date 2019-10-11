d1a715285073b80d783f3ae1d447cc7417e399a9
samuelmo <samuelmo78@gmail.com>
2019-10-11 04:50:21 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    dogs: []
+  };
+  getDog = async () => {
+    const { 
+      data: { message } 
+    } = await axios.get(
+      "https://dog.ceo/api/breeds/image/random"
+      );
+    this.setState({
+      dogs: message, 
+      isLoading: false
+    })
+      //console.log(dog.data.message)
+  };
+  componentDidMount() {
+    this.getDog();
+  }
+  render() {
+    
+    return <div>
+      <img src={this.state.dogs} alt=""/>
+    </div>;
+    
+  }
 }
-
 export default App;
